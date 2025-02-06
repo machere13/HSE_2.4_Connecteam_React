3e006a401cdf4f4c34b67a8cd8719d43e8c48009
machere13 <vladislav.chubarov13@yandex.ru>
2025-02-07 01:00:17 +0300

    feat: added stylesheets architecture

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
