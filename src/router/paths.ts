fd5c6644494766aceb29125d13d9b9ac3fbb19d7
machere13 <vladislav.chubarov13@yandex.ru>
2025-03-07 10:34:58 +0300

    feat: PreviewPage became main

--- src/router/paths.ts
+++ src/router/paths.ts
@@ -1,4 +1,4 @@
-export const PREVIEW_PAGE = '/preview'
+export const PREVIEW_PAGE = '/'
 export const ARTICLES_PAGE = '/articles'
 export const CASES_PAGE = '/cases'
 export const INTERACTIVES_PAGE = '/interactives'
