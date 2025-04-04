64eed582949c8f912b997dd498a8eb48477ae398
machere13 <vladislav.chubarov13@yandex.ru>
2025-04-04 21:18:00 +0300

    feat: testing deploy on netlify

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,34 @@
-import type { NextConfig } from 'next';
-import path from 'path';
+import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: process.env.NETLIFY ? 'export' : undefined,
+  distDir: '.next',
   images: {
     domains: [],
+    unoptimized: process.env.NETLIFY ? true : false
   },
+
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, 'src'),
-    };
+    }
 
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
-    });
+    })
 
-    return config;
+    return config
   },
-};
 
-export default nextConfig;
+  experimental: {
+    externalDir: false,
+    optimizePackageImports: ['@heroicons/react']
+  }
+}
+
+export default nextConfig
