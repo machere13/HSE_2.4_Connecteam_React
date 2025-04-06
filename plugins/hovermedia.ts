a1156f3978157867cf4f27f66c0ff414eff6ffe2
machere13 <vladislav.chubarov13@yandex.ru>
2025-04-06 15:48:25 +0300

    feat: hovermediaplugin ver. 0.2.0

--- plugins/hovermedia.ts
+++ plugins/hovermedia.ts
@@ -1,42 +1,58 @@
-import { PluginCreator, Rule, AtRule } from 'postcss';
+import { PluginCreator, Rule, AtRule, Root } from 'postcss';
 
 const hoverMediaPlugin: PluginCreator<{}> = () => {
   return {
     postcssPlugin: 'postcss-hover-media',
-    Rule(rule) {
-      if (rule.parent && 'name' in rule.parent && rule.parent.name === 'media') return;
+    Once(root: Root) {
+      const hoverRules: Rule[] = [];
+      const activeRules: Rule[] = [];
 
-      if (rule.selector.includes(':hover')) {
+      root.walkRules(rule => {
+        if (rule.selector.includes(':hover') && 
+            !(rule.parent && rule.parent.type === 'atrule')) {
+          hoverRules.push(rule.clone());
+          
+          const activeRule = rule.clone();
+          activeRule.selectors = activeRule.selectors.map(sel => 
+            sel.replace(/:hover/g, ':active')
+          );
+          activeRules.push(activeRule);
+          
+          rule.remove();
+        }
+      });
+
+      if (hoverRules.length > 0) {
         const hoverMedia = new AtRule({
           name: 'media',
           params: '(hover: hover) and (pointer: fine)',
-          raws: { before: '', after: '', between: ' ', semicolon: false }
+          raws: { before: '\n', between: ' ', after: '\n' }
         });
         
-        const hoverRule = rule.clone();
-        hoverRule.raws.before = '\n\t';
-        hoverMedia.append(hoverRule);
+        hoverRules.forEach(rule => {
+          rule.raws.before = '\n\t';
+          hoverMedia.append(rule);
+        });
         hoverMedia.raws.after = '\n';
+        root.append(hoverMedia);
+      }
 
+      if (activeRules.length > 0) {
         const activeMedia = new AtRule({
-          name: 'media',
+          name: 'media', 
           params: '(hover: none) and (pointer: coarse)',
-          raws: { before: '\n', after: '\n', between: ' ', semicolon: false }
+          raws: { before: '\n', between: ' ', after: '\n' }
         });
         
-        const activeRule = rule.clone();
-        activeRule.selectors = activeRule.selectors.map(sel => 
-          sel.replace(/:hover/g, ':active')
-        );
-        activeRule.raws.before = '\n\t';
-        activeMedia.append(activeRule);
-
-        rule.replaceWith(hoverMedia, activeMedia);
+        activeRules.forEach(rule => {
+          rule.raws.before = '\n\t';
+          activeMedia.append(rule);
+        });
+        root.append(activeMedia);
       }
     }
   };
 };
 
 hoverMediaPlugin.postcss = true;
-
 export default hoverMediaPlugin;
