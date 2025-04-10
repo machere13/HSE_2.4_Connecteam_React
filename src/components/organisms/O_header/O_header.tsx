20fe0288c19e134e29fc01df241cb9b7f922954b
machere13 <vladislav.chubarov13@yandex.ru>
2025-04-10 18:58:03 +0300

    feat: changed naming to type "A_NamingType"

--- src/components/organisms/O_header/O_header.tsx
+++ src/components/organisms/O_header/O_header.tsx
@@ -3,16 +3,16 @@ import styles from './O_header.module.css'
 import Link from 'next/link'
 import { ROUTES } from '@/routes'
 import Q_Icon from '@/components/quarks/Q_Icon'
-import C_header_navigation from '@/components/collections/C_header_navigation/C_header_navigation'
+import C_HeaderNavigation from '@/components/collections/C_HeaderNavigation/C_HeaderNavigation'
 
-export default function O_header() {
+export default function O_Header() {
   return (
     <div className={styles.wrapper}>
         <Link href={ROUTES.MAIN} className={styles.logo_wrapper}>
           <Q_Icon name='logoFull' width='200' height='24'/>
         </Link>
         <div className={styles.content}>
-          <C_header_navigation/>
+          <C_HeaderNavigation/>
         </div>
     </div>
   )
