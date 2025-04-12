011c65561b2ab2d125b811a0ac385f48c4cd431c
machere13 <vladislav.chubarov13@yandex.ru>
2025-04-12 22:48:29 +0300

    feat: updated naming constants in Storybook for A_Photo

--- src/stories/atoms/A_photo.stories.tsx
+++ src/stories/atoms/A_photo.stories.tsx
@@ -1,27 +1,27 @@
 import { Meta, StoryFn } from "@storybook/react";
 import { MemoryRouter } from "react-router-dom";
 import A_Photo, { A_PhotoProps } from "@/components/atoms/A_Photo/A_Photo";
-import Photo1 from '@/assets/images/A_Photo/A_Photo_01.webp';
-import Photo2 from '@/assets/images/A_Photo/A_Photo_02.webp';
-import Photo3 from '@/assets/images/A_Photo/A_Photo_03.webp';
+import Photo_01 from '@/assets/images/A_Photo/A_Photo_01.webp';
+import Photo_02 from '@/assets/images/A_Photo/A_Photo_02.webp';
+import Photo_03 from '@/assets/images/A_Photo/A_Photo_03.webp';
 
 const meta: Meta<A_PhotoProps> = {
   title: 'Atoms/A_Photo',
   component: A_Photo,
   argTypes: {
     src: {
-      options: ['photo1', 'photo2', 'photo3'],
+      options: ['photo_01', 'photo_02', 'photo_03'],
       mapping: {
-        photo1: Photo1,
-        photo2: Photo2,
-        photo3: Photo3,
+        photo_01: Photo_01,
+        photo_02: Photo_02,
+        photo_03: Photo_03,
       },
       control: {
         type: 'select',
         labels: {
-          photo1: 'machere13',
-          photo2: 'Vasilisa',
-          photo3: 'Veronika',
+          photo_01: 'machere13',
+          photo_02: 'Vasilisa',
+          photo_03: 'Veronika',
         },
       },
     },
@@ -38,6 +38,6 @@ const Template: StoryFn<A_PhotoProps> = (args: A_PhotoProps) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  src: Photo1,
+  src: Photo_01,
   alt: "Avatar",
 };
