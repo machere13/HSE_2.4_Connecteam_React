616f572ed449d2ed9e040bff4855a12ef5cadd09
machere13 <vladislav.chubarov13@yandex.ru>
2025-05-08 23:49:30 +0300

    fixed: storybook next.config.js -> next.config.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,7 +14,7 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/nextjs',
     options: {
-      nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      nextConfigPath: path.resolve(__dirname, '../next.config.ts'),
     },
   },
   staticDirs: ['../public'],
