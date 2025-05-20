c89b2d300bbbdda4bdf898a58f0bdeee92011dae
machere13 <vladislav.chubarov13@yandex.ru>
2025-05-21 02:04:27 +0300

    feat: added A_SearchButton component

---
+++ src/components/atoms/A_SearchButton/A_SearchButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Q_Icon from '@/components/quarks/Q_Icon'
+
+import styles from './A_SearchButton.module.css'
+
+interface A_SearchButtonProps {
+  onClick: () => void
+}
+
+export default function A_SearchButton({ onClick }: A_SearchButtonProps) {
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault()
+      onClick()
+    }
+  }
+
+  return (
+    <button
+      className={styles.wrapper}
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+      aria-label='Поиск'
+      tabIndex={0}
+    >
+      <Q_Icon name='searchIcon' width='16' height='16' />
+    </button>
+  )
+}
