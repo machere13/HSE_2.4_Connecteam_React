2d44ba3ebd9d5831d65c412c48b9a7df6e0d6663
machere13 <vladislav.chubarov13@yandex.ru>
2025-06-11 22:56:06 +0300

    feat: added W_GeneratorTimeSetting component

--- pages/interactives/generator.tsx
+++ pages/interactives/generator.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 import SO_Header from '@/components/super-organisms/SO_Header/SO_Header'
 import W_GeneratorAmountSetting from '@/components/wrappers/W_GeneratorAmountSetting/W_GeneratorAmountSetting'
+import W_GeneratorTimeSetting from '@/components/wrappers/W_GeneratorTimeSetting/W_GeneratorTimeSetting'
 import { ROUTES } from '@/routes'
 
 export default function GeneratorPage() {
@@ -11,6 +12,10 @@ export default function GeneratorPage() {
       <h1>Генератор идей</h1>
       <W_GeneratorAmountSetting
         options={['Опция 1', 'Опция 2', 'Опция 3']}
+        onSelect={console.log}
+      />
+      <W_GeneratorTimeSetting
+        options={['Опци1я 1', 'Опция 2', 'Опция 3']}
         onSelect={console.log}
       />
       <Link href={ROUTES.INTERACTIVES.INDEX}>Вернуться назад</Link>
