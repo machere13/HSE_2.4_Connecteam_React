2aad1f57a4d69837794c81ca6fd41431a48ea82b
machere13 <vladislav.chubarov13@yandex.ru>
2025-06-14 19:21:00 +0300

    feat: added A_DropdownItem component

---
+++ src/components/atoms/A_DropdownItem/A_DropdownItem.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import cn from 'classnames'
+
+import styles from './A_DropdownItem.module.css'
+
+interface A_DropdownItemProps {
+  title: string
+  isActive?: boolean
+  onClick?: () => void
+}
+
+export default function A_DropdownItem({ title, isActive = false, onClick }: A_DropdownItemProps) {
+  return (
+    <button
+      className={cn(styles.wrapper, isActive && styles.active)}
+      onClick={onClick}
+      type='button'
+    >
+      <span className='text_body_1'>{title}</span>
+    </button>
+  )
+}
