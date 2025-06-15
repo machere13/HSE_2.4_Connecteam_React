08eb73a213fab1614f159bc5a18f5e14113f9361
machere13 <vladislav.chubarov13@yandex.ru>
2025-06-15 21:46:42 +0300

    feat: added getGeneratorCardVariant

--- src/lib/getGeneratorCardVariant.ts
+++ src/lib/getGeneratorCardVariant.ts
@@ -1,31 +1,31 @@
-// interface GetGeneratorCardVariantProps {
-//   itemIndex: number
-// }
+interface GetGeneratorCardVariantProps {
+  itemIndex: number
+}
 
-// interface GeneratorCardVariant {
-//   bgColor: 'gray' | 'pink' | 'purple'
-//   rotate: 'right' | 'none'
-// }
+interface GeneratorCardVariant {
+  bgColor: 'gray' | 'pink' | 'purple'
+  rotate: 'right' | 'none'
+}
 
-// export function getGeneratorCardVariant({
-//   itemIndex,
-// }: GetGeneratorCardVariantProps): GeneratorCardVariant {
-//   if (itemIndex === 0) {
-//     return {
-//       bgColor: 'pink',
-//       rotate: 'right',
-//     }
-//   }
+export function getGeneratorCardVariant({
+  itemIndex,
+}: GetGeneratorCardVariantProps): GeneratorCardVariant {
+  if (itemIndex === 0) {
+    return {
+      bgColor: 'pink',
+      rotate: 'right',
+    }
+  }
 
-//   if (itemIndex === 4) {
-//     return {
-//       bgColor: 'purple',
-//       rotate: 'none',
-//     }
-//   }
+  if (itemIndex === 4) {
+    return {
+      bgColor: 'purple',
+      rotate: 'none',
+    }
+  }
 
-//   return {
-//     bgColor: 'gray',
-//     rotate: 'none',
-//   }
-// }
+  return {
+    bgColor: 'gray',
+    rotate: 'none',
+  }
+}
