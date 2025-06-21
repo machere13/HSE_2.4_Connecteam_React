975dfe386659c5c070d647e2a65e3d3751e10bd7
machere13 <vladislav.chubarov13@yandex.ru>
2025-06-21 04:44:26 +0300

    feat: added C_PreviewCasesCards component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import A_Cursor from '@/components/atoms/A_Cursor/A_Cursor'
 import { firstMainPageCursors } from '@/components/atoms/A_Cursor/data/cursors'
+import C_PreviewCasesCards from '@/components/collections/C_PreviewCasesCards/C_PreviewCasesCards'
 import O_Footer from '@/components/organisms/O_Footer/O_Footer'
 import Q_Grid from '@/components/quarks/Q_Grid/Q_Grid'
 import SO_Header from '@/components/super-organisms/SO_Header/SO_Header'
@@ -12,6 +13,7 @@ export default function MainPage() {
       <Meta title='Connecteam' description='Медиа-сервис для IT-специалистов' />
       <SO_Header />
       <W_PreviewArticlesCardsBlock />
+      <C_PreviewCasesCards />
       <Q_Grid variant='gray' />
       <A_Cursor cursors={firstMainPageCursors} />
       <O_Footer />
---
+++ src/components/collections/C_PreviewCasesCards/C_PreviewCasesCards.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+
+import cn from 'classnames'
+import Link from 'next/link'
+
+import { getCases } from '@/api/getCases'
+import A_ComingSoonTag from '@/components/atoms/A_ComingSoonTag/A_ComingSoonTag'
+import Q_ThunderIconTag from '@/components/quarks/Q_ThunderIconTag/Q_ThunderIconTag'
+import { ROUTES } from '@/routes'
+
+import styles from './C_PreviewCasesCards.module.css'
+
+import type { CaseData } from '@/types/case'
+
+export default function C_PreviewCasesCards() {
+  const [cases, setCases] = useState<CaseData[]>([])
+
+  useEffect(() => {
+    const fetchCases = async () => {
+      const casesData = await getCases()
+      setCases(casesData)
+    }
+
+    fetchCases()
+  }, [])
+
+  const getRandomCases = (count = 2): CaseData[] => {
+    const shuffled = [...cases].sort(() => 0.5 - Math.random())
+    return shuffled.slice(0, count)
+  }
+
+  const previewCases = getRandomCases(2)
+
+  return (
+    <div className={styles.wrapper}>
+      {previewCases.map(caseItem => {
+        const isImageBackground =
+          caseItem.cardDisplay.background.startsWith('http') ||
+          caseItem.cardDisplay.background.startsWith('/')
+        const backgroundStyle = isImageBackground
+          ? { backgroundImage: `url(${caseItem.cardDisplay.background})` }
+          : { backgroundColor: `var(--color-main-${caseItem.cardDisplay.background})` }
+
+        return (
+          <Link
+            key={caseItem.id}
+            href={ROUTES.CASES.bySlug(caseItem.slug)}
+            className={cn(styles.case_card, styles[caseItem.cardDisplay.rotate])}
+            style={backgroundStyle}
+          >
+            <div className={styles.card_content}>
+              <div className={styles.card_additional_info}>
+                {caseItem.cardDisplay.hasIcon && <Q_ThunderIconTag />}
+                {caseItem.cardDisplay.comingSoon && <A_ComingSoonTag />}
+              </div>
+              <div className={styles.card_text}>
+                <h5>{caseItem.title}</h5>
+              </div>
+            </div>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
