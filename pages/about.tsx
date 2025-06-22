fe7f0a4ea3cc184519c0a549a65575413dc4fc39
machere13 <vladislav.chubarov13@yandex.ru>
2025-06-22 13:54:09 +0300

    feat: added M_AboutArticlesDirectionPlate component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
+import M_AboutArticlesDirectionPlate from '@/components/molecules/M_AboutArticlesDirectionPlate/M_AboutArticlesDirectionPlate'
+import O_Footer from '@/components/organisms/O_Footer/O_Footer'
 import SO_Header from '@/components/super-organisms/SO_Header/SO_Header'
 import W_AboutGeneratorDirectionSection from '@/components/wrappers/W_AboutGeneratorDirectionSection/W_AboutGeneratorDirectionSection'
 
 export default function AboutPage() {
   return (
-    <div>
+    <div className='page'>
       <SO_Header />
       <W_AboutGeneratorDirectionSection />
+      <div className='preview_content_wrapper'>
+        <M_AboutArticlesDirectionPlate />
+      </div>
+      <O_Footer />
     </div>
   )
 }
