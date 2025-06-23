44794549bf14ad120d9621139f046f5982319bc7
machere13 <vladislav.chubarov13@yandex.ru>
2025-06-23 19:20:47 +0300

    feat: added real db for tests

--- pages/interactives/index.tsx
+++ pages/interactives/index.tsx
@@ -1,5 +1,6 @@
 import { getTests } from '@/api/getTests'
 import M_InteractivesGeneratorDirectionPlate from '@/components/molecules/M_InteractivesGeneratorDirectionPlate/M_InteractivesGeneratorDirectionPlate'
+import O_Footer from '@/components/organisms/O_Footer/O_Footer'
 import Q_Grid from '@/components/quarks/Q_Grid/Q_Grid'
 import SO_Header from '@/components/super-organisms/SO_Header/SO_Header'
 import W_StickersContainer from '@/components/wrappers/W_StickersContainer/W_StickersContainer'
@@ -26,6 +27,7 @@ export default function InteractivesPage({ tests }: { tests: TestData[] }) {
         <W_TestCardsWithTitle tests={tests} />
       </div>
       <W_StickersContainer />
+      <O_Footer />
     </div>
   )
 }
